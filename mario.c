49f04094dfecba556f3834e12adcf1f733ec5c46
curious-the-cat <curious-the-cat@users.noreply.github.com>
2023-03-12 20:52:23 -0600

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -12,17 +12,17 @@ int main(void)
     while (height > 8 || height < 1);
 
 
-for (int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
 
-        for (int k = height - 1; k > i; k--)
+        for (int k = height - 1; k > i; k--) // moves pyramid so it's right-aligned
         {
             // e.g. print 4 ., next row print 3 ., next row print 2 ., next row print 1 ., next row print 0 .
             // start at k = height - 1 then - 1 each row until k = 0
             printf(" ");
         }
 
-        for (int j = 0; j < i+1; j++) // add 1 # to each row, then go to new line for left-aligned pyramid
+        for (int j = 0; j < i + 1; j++) // add 1 # to each row, then go to new line for left-aligned pyramid
         {
             printf("#");
         }
