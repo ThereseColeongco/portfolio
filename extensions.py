efe31847da1b4bd037932b83691cb6578dc64db9
curious-the-cat <curious-the-cat@users.noreply.github.com>
2023-03-27 20:57:16 -0600

    automated commit by check50 [check50=True]

--- extensions.py
+++ extensions.py
@@ -1,5 +1,19 @@
 file = input("Enter a file name in the format name_of_file.extension : ").lower().strip()
-extension = file.split('.')[1]
+
+extension = file.split('.')[1:]
+
+#need a less verbose way of accounting for these cases:
+'''
+:( input of test.txt.pdf, with one extra extension, yields output of application/pdf
+    expected "application/pd...", not "text/plain\n"
+
+happens b/c if e.g. extension == "pdf", it will print application/pdf even if after
+'''
+'''
+:( input of myfile, with no extension, yields output of application/octet-stream
+    expected "application/oc...", not "Traceback (mos..."
+'''
+
 if extension == "jpeg" or extension == "jpg":
     print("image/jpeg")
 elif extension == "pdf":
