1365b0c3d3dc9e447be8cf1f3326967f1903bf56
curious-the-cat <curious-the-cat@users.noreply.github.com>
2023-03-27 20:33:31 -0600

    automated commit by check50 [check50=True]

---
+++ bank.py
@@ -0,0 +1,8 @@
+greeting = input("Send a greeting: ").lower().split(" ", 1)[0]
+
+if greeting == "hello" or greeting == "hello,":
+    print("$0")
+elif greeting[0] == "h":
+    print("$20")
+else:
+    print("$100")
