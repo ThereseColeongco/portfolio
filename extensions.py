d7f71c7a3eff9a04cc4b4cabd468da432c1b58da
curious-the-cat <curious-the-cat@users.noreply.github.com>
2023-03-28 18:15:39 -0600

    automated commit by submit50

--- extensions.py
+++ extensions.py
@@ -3,21 +3,6 @@ file = input("Enter a file name in the format name_of_file.extension : ").lower(
 # this extension definition (instead of original file.split(.)[1]) fixes the following errors:
 extension = file.split('.')[-1]
 
-'''
-:( input of test.txt.pdf, with one extra extension, yields output of application/pdf
-    expected "application/pd...", not "text/plain\n"
-
-happens b/c if e.g. extension == "pdf", it will print application/pdf even if after
-'''
-'''
-:( input of myfile, with no extension, yields output of application/octet-stream
-    expected "application/oc...", not "Traceback (mos..."
-
-happens b/c
-'''
-# but b/c it puts everything in a list, even if it's just 1 extension,
-# i can't check it against strings like "jpeg", etc.
-
 if extension == "jpeg" or extension == "jpg":
     print("image/jpeg")
 elif extension == "pdf":
