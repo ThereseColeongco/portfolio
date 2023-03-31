7bce1be589019219cfb817408d1d6d63554481e6
curious-the-cat <curious-the-cat@users.noreply.github.com>
2023-03-30 18:30:04 -0600

    automated commit by check50 [check50=True]

--- twttr.py
+++ twttr.py
@@ -2,11 +2,11 @@ text = input("Input: ")
 vowels = 'AEIOUaeiou'
 '''
 for i in range(len(vowels)):
-     text.replace(vowels[i], '')
+     text = text.replace(vowels[i], '')
 
 print(text)
 '''
 for vowel in vowels:
-    print(text.replace(vowel, ''))
+    text = text.replace(vowel, '')
 
 print(text)
