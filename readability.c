3be68bf791096d83c8e0110a3bf92d4ccbfc1180
curious-the-cat <curious-the-cat@users.noreply.github.com>
2023-06-18 16:28:20 -0600

    automated commit by submit50

--- readability.c
+++ readability.c
@@ -3,10 +3,6 @@
 #include <math.h>
 #include <stdio.h>
 
-// letter = any lowercase or uppercase character from A to Z
-// word = any sequence of characters separated by spaces
-// sentence = any occurrence of a ., !, or ? is end of sentence
-
 int count_letters(string text);
 int count_words(string text);
 int count_sentences(string text);
@@ -24,6 +20,7 @@ int main(void)
     // Coleman-Liau index
     float index = (0.0588 * L) - (0.296 * S) - 15.8;
 
+    // reports grade level of text to nearest whole number
     if (index >= 16)
     {
         printf("Grade 16+\n");
@@ -43,6 +40,7 @@ int count_letters(string text)
     int count_letter = 0;
     int n = 0;
 
+    // iterates through text. for every character in text, if character is a letter in the alphabet, add 1 to letter counter. if character is not a letter in the alphabet, add 0 to letter counter.
     while (text[n] != '\0')
     {
         if (isalpha(text[n]))
@@ -55,6 +53,7 @@ int count_letters(string text)
         }
         n++;
     }
+
     return count_letter;
 }
 
@@ -63,6 +62,7 @@ int count_words(string text)
     int count_words = 0;
     int n = 0;
 
+    // iterates through text. for every character in text, if character is a space, that means we've just passed a word, so add 1 to word counter.
     while (text[n] != '\0')
     {
         if (text[n] == ' ')
@@ -71,7 +71,10 @@ int count_words(string text)
         }
         n++;
     }
+
+    // add 1 extra to word counter b/c at the end of the text, there's 1 more word but no space to indicate a word.
     count_words++;
+
     return count_words;
 }
 
@@ -80,6 +83,7 @@ int count_sentences(string text)
     int count_sentences = 0;
     int n = 0;
 
+    // iterates through text. for every character in text, check if the character is a ., ?, or !. if it's any of those, add 1 to sentence counter.
     while (text[n] != '\0')
     {
         if (text[n] == '.' || text[n] == '?' || text[n] == '!')
@@ -88,5 +92,6 @@ int count_sentences(string text)
         }
         n++;
     }
+
     return count_sentences;
 }
