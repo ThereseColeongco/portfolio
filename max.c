90eeeb4a058f9c4b5f03b791a55da313f9260e1b
curious-the-cat <curious-the-cat@users.noreply.github.com>
2023-06-22 12:23:22 -0600

    automated commit by check50 [check50=True]

--- max.c
+++ max.c
@@ -30,7 +30,7 @@ int max(int array[], int n)
     int max = array[0];
     for (int i = 0; i < n; i++)
     {
-        if (array[i] > array[i - 1])
+        if (array[i] > array[0])
         {
             max = array[i];
         }
@@ -38,9 +38,13 @@ int max(int array[], int n)
     return max;
 }
 
+
+
 //0, 1, 3
     // when 3 is array[i], there is no array[i + 1] to compare to
 //-10, -50, -100
     // when -50 is array[i], it IS greater than -100 aka array[i + 1]
     // when -100 is array[i], there's no array[i + 1] to compare to
-// solution: change the sign to array[i - 1]?
+// solution: change the sign to array[i - 1]
+// problem with solution: when i = 0, aren't i accessing smth that's not in the array (i.e. array[-1])?
+// solution: just compare every other element in array to 
