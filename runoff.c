7a92d437b8cfe11372a50c9a11652cfce42cd194
curious-the-cat <curious-the-cat@users.noreply.github.com>
2023-06-26 19:35:46 -0600

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -243,7 +243,7 @@ bool print_winner(void)
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes > (voter_count / 2))
+        if ((candidates[i].votes / voter_count) > 0.5)
         {
             printf("%i\n", candidates[i].votes);
             printf("%s\n", candidates[i].name);
