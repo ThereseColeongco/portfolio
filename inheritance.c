577f32e6ca874f8ccc6e5337edf4cadb8afe0536
curious-the-cat <curious-the-cat@users.noreply.github.com>
2023-07-23 22:20:26 -0600

    automated commit by check50 [check50=True]

--- inheritance.c
+++ inheritance.c
@@ -58,7 +58,7 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
-    person *new = malloc(sizeof(person));
+    person *p = malloc(sizeof(person));
 
     // If there are still generations left to create
     if (generations > 1)
@@ -68,31 +68,29 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        new->parents[0] = parent0;
-        new->parents[1] = parent1;
+        p->parents[0] = parent0;
+        p->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
         // rand() % 2 = index of an allele in char alleles[2];
 
-        new->alleles[0] = new->parents[0]->alleles[rand() % 2];
-        new->alleles[1] = new->parents[1]->alleles[rand() % 2];
+        p->alleles[0] = p->parents[0]->alleles[rand() % 2];
+        p->alleles[1] = p->parents[1]->alleles[rand() % 2];
     }
     // If there are no generations left to create
     else
     {
         // TODO: Set parent pointers to NULL
-        new->parents[0] = NULL;
-        new->parents[1] = NULL;
+        p->parents[0] = NULL;
+        p->parents[1] = NULL;
 
         // TODO: Randomly assign alleles
-        new->parents[0]->alleles[0] = random_allele();
-        new->parents[0]->alleles[1] = random_allele();
-        new->parents[1]->alleles[0] = random_allele();
-        new->parents[1]->alleles[0] = random_allele();
+        p->alleles[0] = random_allele();
+        p->alleles[1] = random_allele();
     }
 
     // TODO: Return newly created person
-    return new;
+    return p;
 }
 
 // Free `p` and all ancestors of `p`.
