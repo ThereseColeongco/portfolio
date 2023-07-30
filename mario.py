6207dd7250b464ca0fbba8c2ac57992852f1d19f
curious-the-cat <curious-the-cat@users.noreply.github.com>
2023-07-30 17:29:37 -0600

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -5,14 +5,13 @@ while True:
     if 1 <= height <= 8:
         break
 
-# this code makes a left-aligned pyramid.
 for j in reversed(range(height)):
     k = height - j
     print(" " * j + "#" * k + "  " + "#" * k)
 
-'''
+"""
 for both pyramids:
 start w/ 8, you want to subtract 7 for the hash
 start w/ 7, you want to subtract 6 for the hash
 and so on.
-'''
+"""
